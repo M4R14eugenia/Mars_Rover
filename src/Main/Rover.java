2481383d0f048b55ab9978d8232d5d97c8470cde
Maria Santos <mariaeugenia.fs@hotmail.com>
2024-11-14 10:21:55 +0000

    "feat: implement Movable interface in the Rover class"

--- src/Main/Rover.java
+++ src/Main/Rover.java
@@ -1,2 +1,11 @@
-public class Rover {
+public class Rover implements Movable{
+    @Override
+    public Directions rotate(Instructions instructions, Position position) {
+        return null;
+    }
+
+    @Override
+    public Directions move(Instructions instructions) {
+        return null;
+    }
 }
