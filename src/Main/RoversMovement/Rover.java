45a46149eac607eba05e81fbad9067afaa650219
Maria Santos <mariaeugenia.fs@hotmail.com>
2024-11-14 19:58:51 +0000

    "feat: add logic to method turnRight"

--- src/Main/RoversMovement/Rover.java
+++ src/Main/RoversMovement/Rover.java
@@ -6,6 +6,7 @@ public class Rover implements Movable{
     Position position;
     Directions direction;
     Instructions instructions;
+    Rover rover;
 
     public Rover(Position position) {
         this.position = position;
@@ -15,8 +16,18 @@ public class Rover implements Movable{
     public Directions turnRight(Instructions instructions, Position position) {
         //if position is
          //North - W; West - S; South - East; East - North
+        if(instructions == Instructions.L) {
+            if (position.getFacing() == Directions.N) {
+                return Directions.W;
+            } else if (position.getFacing() == Directions.W) {
+                return Directions.S;
+            }else if (position.getFacing() == Directions.S){
+                return Directions.E;
+            }else if (position.getFacing() == Directions.E){
+                return Directions.N;
+            }
 
-
+        }
         return null;}
     public Directions turnLeft(Instructions instructions, Position position) {
       //switch North - E; East - South; S- West; W -North;
