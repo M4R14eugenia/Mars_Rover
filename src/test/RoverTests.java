f85ba572371b7ec44217d4b9d4fe3962c20aa497
Maria Santos <mariaeugenia.fs@hotmail.com>
2024-11-14 22:11:57 +0000

    "feat: add tests for turning left"

--- src/test/RoverTests.java
+++ src/test/RoverTests.java
@@ -24,7 +24,7 @@ public class RoverTests {
 
 
 @Test
-@DisplayName("Tests rotate method returns right position when turning right from any point")
+@DisplayName("Tests method returns right position when turning right from any point")
 void testTurnRight() {
     // Arrange
     Position position = new Position(3,2,Directions.N);
@@ -49,4 +49,42 @@ void testTurnRight() {
             () -> assertEquals(position2.getFacing(),Directions.W),
             () -> assertEquals(position3.getFacing(), Directions.N));
 }
+
+    @Test
+    @DisplayName("Tests method returns point West when turning left from North")
+    void testTurnLeftFromNorth() {
+        // Arrange
+        Position position = new Position(3, 2, Directions.N);
+        Rover rover = new Rover(position);
+        // Act
+        rover.turnLeft(Instructions.L);
+        // Assert
+        assertEquals(Directions.W, position.getFacing());
+    }
+    @Test
+    @DisplayName("Tests method returns right position when turning left from any point")
+    void testTurnLeft() {
+        // Arrange
+        Position position = new Position(3,2,Directions.N);
+        Rover rover = new Rover(position);
+        Position position1 = new Position(3,4,Directions.E);
+        Rover roverOne = new Rover(position1);
+        Position position2 = new Position(3,1,Directions.S);
+        Rover roverTwo = new Rover(position2);
+        Position position3 = new Position(4,2,Directions.W);
+        Rover roverThree = new Rover(position3);
+
+        // Act
+        rover.turnLeft(Instructions.L);
+        roverOne.turnLeft(Instructions.L);
+        roverTwo.turnLeft(Instructions.L);
+        roverThree.turnLeft(Instructions.L);
+
+        // Assert
+        assertAll("grouped assertions of turning right",
+                () -> assertEquals(position.getFacing(), Directions.W),
+                () -> assertEquals(position1.getFacing(), Directions.N),
+                () -> assertEquals(position2.getFacing(),Directions.E),
+                () -> assertEquals(position3.getFacing(), Directions.S));
+    }
 }
