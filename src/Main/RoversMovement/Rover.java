efc666e5971d0b08b7c0a504a89de5c961cf2020
Maria Santos <mariaeugenia.fs@hotmail.com>
2024-11-14 15:24:24 +0000

    "feat: add pseudocode to Rover methods"

--- src/Main/RoversMovement/Rover.java
+++ src/Main/RoversMovement/Rover.java
@@ -7,17 +7,28 @@ public class Rover implements Movable{
     Directions direction;
     Instructions instructions;
 
-
-
-
-    @Override
-    public Directions rotate(Instructions instructions, Position position) {
-
-        return null;
+    public Rover(Position position) {
+        this.position = position;
     }
 
     @Override
+    public Directions turnRight(Instructions instructions, Position position) {
+        //if position is
+         //North - W; West - S; South - East; East - North
+
+
+        return null;}
+    public Directions turnLeft(Instructions instructions, Position position) {
+      //switch North - E; East - South; S- West; W -North;
+
+        return null;}
+
+    @Override
     public Directions move(Instructions instructions, Position position) {
+        //if "M"
+        //get position
+        //if facing North = y+1; if facing South y-1; if facing east x-1; if facing west x+1
+
         return null;
     }
 }
